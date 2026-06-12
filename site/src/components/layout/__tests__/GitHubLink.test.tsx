bf3f96612fdbd60cd88ce52b188bcd625e72f18f
Jeramie <jeramie@protonmail.com>
2026-06-12 02:39:58 -0700

    test(layout): pin GitHubLink repo URL with a literal assertion

--- site/src/components/layout/__tests__/GitHubLink.test.tsx
+++ site/src/components/layout/__tests__/GitHubLink.test.tsx
@@ -3,6 +3,10 @@ import { render } from "@testing-library/react";
 import { GitHubLink, GITHUB_URL } from "../GitHubLink";
 
 describe("GitHubLink", () => {
+  it("points at the canonical repo URL", () => {
+    expect(GITHUB_URL).toBe("https://github.com/EternityX/rustybin/");
+  });
+
   it("links to the repo and opens safely in a new tab", () => {
     const { getByRole } = render(<GitHubLink />);
     const link = getByRole("link", { name: /github/i }) as HTMLAnchorElement;
